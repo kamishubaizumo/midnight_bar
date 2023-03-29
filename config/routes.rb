05dfaf2b1ef0e6fcbe6a040d0794d4c1571f556b
kamishubaizumo <kamishubaizumo@yahoo.co.jp>
2023-03-29 23:52:26 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,29 @@
+Rails.application.routes.draw do
+  
+
+      # 管理者用
+      # URL /admin/sign_in ...
+    devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+      sessions: "admin/sessions"
+    }
+
+    namespace :admin do
+      root to: "homes#home"
+    end
+
+
+      # 顧客用
+      # URL /customers/sign_in ...
+    devise_for :customers,skip: [:passwords], controllers: {
+      registrations: "public/registrations",
+      sessions: 'public/sessions'
+    } 
+
+
+     scope module: :public do
+      root to: 'homes#top'
+      get "homes/about"
+    end
+
+
+end
